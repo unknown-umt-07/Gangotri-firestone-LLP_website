f124e16200dddb117b9e18fa57985faa1ab0a5ba
unknown-umt-07 <prakash3940c@gmail.com>
2026-07-20 08:46:15 +0530

    Fix render_build.sh: use && to chain pip commands properly

--- render_build.sh
+++ render_build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # Render build helper: upgrade packaging tools and install requirements
 set -e
-python -m pip install -U pip setuptools wheel
-pip install -r requirements.txt
+python -m pip install -U pip setuptools wheel && pip install -r requirements.txt
